dcd36b7454ff50f7d0aefb2b05a26b655eb8b1dd
Benjamin Zagorsky <iamzags@gmail.com>
2014-07-30 12:24:24 -0400

    refactors common logic from decorators and mixins into new verification module in doing so, applies the improved error message to mixins as well (previous commit only applied it to decorators)

--- django_auth_lti/decorators.py
+++ django_auth_lti/decorators.py
@@ -1,31 +1,17 @@
 from functools import wraps
-from django.core.exceptions import PermissionDenied, ImproperlyConfigured
 from django.utils.decorators import available_attrs
 from django.shortcuts import redirect
 from django.core.urlresolvers import reverse_lazy
+from django_auth_lti.verification import is_allowed
 
 
 def lti_role_required(allowed_roles, redirect_url=reverse_lazy('not_authorized'), raise_exception=False):
     def decorator(view_func):
         @wraps(view_func, assigned=available_attrs(view_func))
         def _wrapped_view(request, *args, **kwargs):
-            if not isinstance(allowed_roles, (list, tuple)):
-                allowed = (allowed_roles, )
-            else:
-                allowed = allowed_roles
-
-            lti_params = request.session.get('LTI_LAUNCH', None)
-            if lti_params is None:
-                # If this is raised, then likely the project doesn't have
-                # the correct settings or is being run outside of an lti context
-                raise ImproperlyConfigured("No LTI_LAUNCH vale found in session")
-            user_roles = lti_params.get('roles', [])
-            if set(allowed) & set(user_roles):
+            if is_allowed(request, allowed_roles, raise_exception):
                 return view_func(request, *args, **kwargs)
-
-            if raise_exception:
-                raise PermissionDenied
-
+            
             return redirect(redirect_url)
         return _wrapped_view
     return decorator
