cfeb44107769765d6a97f1079ae987506cab5a14
Douglas Hall <douglas_hall@harvard.edu>
2015-04-30 14:28:36 +0000

    Incorporating changes for PR feedback

--- django_auth_lti/verification.py
+++ django_auth_lti/verification.py
@@ -23,6 +23,12 @@ def is_allowed(request, allowed_roles, raise_exception):
     return is_user_allowed
 
 
-def has_lti_role(request, role):
-    roles = request.session['LTI_LAUNCH'].get('roles', [])
-    return set(roles) & set([role])
+def has_lti_roles(request, roles):
+    # TODO: Refactor this common code here and above to keep it DRY
+    lti_launch = request.session.get('LTI_LAUNCH', None)
+    if not isinstance(lti_launch, dict):
+        # If this is raised, then likely the project doesn't have
+        # the correct settings or is being run outside of an lti context
+        raise ImproperlyConfigured("No LTI_LAUNCH found in session")
+    user_roles = lti_launch.get('roles', [])
+    return bool(set(user_roles) & set(roles))
