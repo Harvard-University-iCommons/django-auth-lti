59c655ebd1c039995a23dd4df74f7ca1c4d8a510
Douglas Hall <douglas_hall@harvard.edu>
2015-05-15 17:16:04 +0000

    Added exclude_resource_link_id kwarg to monkey patched reverse function

--- django_auth_lti/patch_reverse.py
+++ django_auth_lti/patch_reverse.py
@@ -15,14 +15,28 @@ django_reverse = None
 def reverse(*args, **kwargs):
     """
     Call django's reverse function and append the current resource_link_id as a query parameter
+
+    :param kwargs['exclude_resource_link_id']: Do not add the resource link id as a query parameter
+    :returns Django named url
     """
     request = get_current_request()
+
+    # Check for custom exclude_resource
+    exclude_resource_link_id = False
+    if 'exclude_resource_link_id' in kwargs:
+        exclude_resource_link_id = kwargs['exclude_resource_link_id']
+        del kwargs['exclude_resource_link_id']
+
     url = django_reverse(*args, **kwargs)
-    parsed = urlparse(url)
-    query = parse_qs(parsed.query)
-    if 'resource_link_id' not in query.keys():
-        query['resource_link_id'] = request.LTI.get('resource_link_id')
-        url = urlunparse((parsed.scheme, parsed.netloc, parsed.path, parsed.params, urlencode(query), parsed.fragment))
+    if not exclude_resource_link_id:
+        # Append resource_link_id query param if exclude_resource_link_id kwarg was not passed or is False
+        parsed = urlparse(url)
+        query = parse_qs(parsed.query)
+        if 'resource_link_id' not in query.keys():
+            query['resource_link_id'] = request.LTI.get('resource_link_id')
+            url = urlunparse(
+                (parsed.scheme, parsed.netloc, parsed.path, parsed.params, urlencode(query), parsed.fragment)
+            )
     return url
 
 
