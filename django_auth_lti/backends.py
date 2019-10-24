65e1edc5d8c586511354693dd3f3682ec419be40
Chris-Thornton-Harvard <christopher_thornton@harvard.edu>
2019-10-24 12:49:48 -0400

    Removed import of oauth2

--- django_auth_lti/backends.py
+++ django_auth_lti/backends.py
@@ -1,7 +1,6 @@
 import logging
 from time import time
 
-import oauth2
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
